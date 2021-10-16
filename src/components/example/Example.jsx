ecec25a1a782d6d8ac94fec04f20cb8e6fe0dedd
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-17 02:00:33 +0300

    added Avatar

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,14 +3,23 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
+import Head from "../header/Header";
+import Avatar from "../avatar/Avatar";
+
+
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
+      <Head/>
+      <Avatar verified={true} />
+      
       <Container className={classNames(styles.container)} maxWidth="xl">
+
         <Paper className={classNames(styles.paper)}>
+
           <Link href="/about" color="secondary">
-            Hi
+
           </Link>
         </Paper>
       </Container>
