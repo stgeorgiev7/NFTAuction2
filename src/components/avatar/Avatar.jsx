26b67d4595ebf2867e5bd0d71a36c355775ff1fe
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-17 02:14:29 +0300

    adjusted width and height

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,13 +2,12 @@ import React, {useState} from "react";
 import classNames from "classnames";
 import styles from "./Avatar.module.scss";
 import defaultImg from "./image/avatar.png";
-import { Avatar as AvatarLogo} from "@mui/material";
 
 export default function Avatar({ url, size = { width: 90, height: 90 }, verified = false }) {
     const [avatarPic, setAvatarPic] = useState(url ? url:defaultImg.src);
 
     return (
-        <div className={classNames(styles.avatar)}>
+        <div className={classNames(styles.avatar)} style={{ width: size.width, height: size.height }}>
             <img src={avatarPic} style={{ width: size.width, height: size.height }} className={classNames(styles.image)} />
             {verified ? <span className={classNames(styles.badge)}></span> : null}
         </div>
