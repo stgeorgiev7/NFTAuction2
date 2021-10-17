861193cf67a59ca01b9e95a4233ffe781f2dda6e
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-17 03:24:37 +0300

    adjusted size

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,13 +3,13 @@ import classNames from "classnames";
 import styles from "./Avatar.module.scss";
 import defaultImg from "./image/avatar.png";
 
-export default function Avatar({ url, size = { width: 90, height: 90 }, verified = false }) {
+export default function Avatar({ url, size = { width: '90px', height: '90px' }, verified = false }) {
     const [avatarPic, setAvatarPic] = useState(url ? url:defaultImg.src);
 
     return (
         <div className={classNames(styles.avatar)} >
-            <img src={avatarPic} style={{ width: size.width, height: size.height }} className={classNames(styles.image)} />
-            {verified ? <span className={classNames(styles.badge)} ></span> : null}
+            <img src={avatarPic} style={{ width: size, height: size }} className={classNames(styles.image)} />
+            {verified ? <span className={classNames(styles.badge)}></span> : null}
         </div>
     )
 };
